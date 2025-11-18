8830c544d794a81e9b72f82043545ab6d8eec332
Gulcan Damdelen <gdamdelen@ccc.net>
2025-11-18 12:36:56 +0200

    Add DropdownComponent and integrate with GameComponent; update templates and styles

--- apps/frontend/src/app/game/game.component.ts
+++ apps/frontend/src/app/game/game.component.ts
@@ -1,15 +1,17 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
 import { TimerComponent } from '../timer/timer.component';
+import { DropdownComponent } from '../dropdown/dropdown.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [TimerComponent],
+  imports: [TimerComponent, DropdownComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent {
   img = viewChild<ElementRef>('img');
+  showDropdown = signal<boolean>(false);
 
   onClick(event: MouseEvent) {
      const target = event.target as HTMLElement;
@@ -21,6 +23,8 @@ export class GameComponent {
 
      const xNormalized = posX / rect.width;
      const yNormalized = posY / rect.height; 
+
+     this.showDropdown.set(!this.showDropdown());
 
      //TODO: show dropdown for character selection at (posX, posY)
      //TODO: send (xNormalized, yNormalized) to backend for character validation
