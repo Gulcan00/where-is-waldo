2a55b9959d3dc1cc6fc4d0cfbddbe639b6910b2f
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-02 10:21:50 +0200

    Validate request body using express validator

--- apps/backend/src/controllers/game.controller.ts
+++ apps/backend/src/controllers/game.controller.ts
@@ -1,47 +1,66 @@
 import type { NextFunction, Request, Response } from "express";
+import { body, validationResult } from 'express-validator';
 import { prisma } from "../prisma/client";
 
-const validate = async (req: Request, res: Response, next: NextFunction) => {
-    // TODO validate req body using express validation
-    const { characterId, positionX, positionY, imgUrl } = req.body;
-    try {
-        const position = await prisma.position.findFirstOrThrow({
-            where: {
-                characterId,
-                imgUrl
+const validate = [
+        body('characterId').isNumeric().withMessage('characterId is required'),
+        body('positionX').isNumeric().withMessage('positionX is required'),
+        body('positionY').isNumeric().withMessage('positionY is required'),
+        body('imgUrl').notEmpty().withMessage('imgUrl is required')
+        .matches(/^\/assets\/images\/scenes\/scene[1|2|3].jpeg/).withMessage('Invalid image path'),
+        async (req: Request, res: Response, next: NextFunction) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({errors: errors.array()});
             }
-        });
+            const { characterId, positionX, positionY, imgUrl } = req.body;
+            try {
+                const position = await prisma.position.findFirstOrThrow({
+                    where: {
+                        characterId,
+                        imgUrl
+                    }
+                });
 
-        const distance = Math.sqrt(Math.pow(positionX - Number(position.xCoordinate), 2) + Math.pow(positionY - Number(position.yCoordinate), 2));
+                const distance = Math.sqrt(Math.pow(positionX - Number(position.xCoordinate), 2) + Math.pow(positionY - Number(position.yCoordinate), 2));
 
-        if (distance <= Number(position.radius)) {
-            return res.json(true);
-        } 
+                if (distance <= Number(position.radius)) {
+                    return res.json(true);
+                } 
 
-        return res.json(false);
-    } catch(err) {
-        next(err);
+                return res.json(false);
+            } catch(err) {
+                next(err);
+            }
     }
-}
+];
 
 const startGame = (req: Request, res: Response) => {
     req.session.startTime = Date.now();
     return res.json("Game started");
 }
 
-const endGame = async (req: Request, res: Response) => {
-    const start = req.session.startTime;
-    if (!start) return res.status(400).json({error: "Game not started"});
-
-    const name = req.body.name;
-    await prisma.score.create({
-        data: {
-            name, 
-            sid: req.sessionID,
-            time: Date.now() - start
+const endGame = [
+        body('name').trim().isAlphanumeric().withMessage('Name is required'),
+        async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
         }
-    });
-    return res.json("Game ended");
-}
+
+        const start = req.session.startTime;
+        if (!start) return res.status(400).json({error: "Game not started"});
+
+        const { name } = req.body;
+        await prisma.score.create({
+            data: {
+                name, 
+                sid: req.sessionID,
+                time: Date.now() - start
+            }
+        });
+        return res.json("Game ended");
+    }
+];
 
 export { validate, startGame, endGame };
