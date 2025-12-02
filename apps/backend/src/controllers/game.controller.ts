e5fd653196858643c0cc144fb3529d4e94ee4a25
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-02 09:54:30 +0200

    Implement startGame, endGame endpoints

--- apps/backend/src/controllers/game.controller.ts
+++ apps/backend/src/controllers/game.controller.ts
@@ -4,20 +4,44 @@ import { prisma } from "../prisma/client";
 const validate = async (req: Request, res: Response, next: NextFunction) => {
     // TODO validate req body using express validation
     const { characterId, positionX, positionY, imgUrl } = req.body;
-    const position = await prisma.position.findFirstOrThrow({
-        where: {
-            characterId,
-            imgUrl
-        }
-    });
+    try {
+        const position = await prisma.position.findFirstOrThrow({
+            where: {
+                characterId,
+                imgUrl
+            }
+        });
 
-    const distance = Math.sqrt(Math.pow(positionX - Number(position.xCoordinate), 2) + Math.pow(positionY - Number(position.yCoordinate), 2));
+        const distance = Math.sqrt(Math.pow(positionX - Number(position.xCoordinate), 2) + Math.pow(positionY - Number(position.yCoordinate), 2));
 
-    if (distance <= Number(position.radius)) {
-        return res.json(true);
-    } 
+        if (distance <= Number(position.radius)) {
+            return res.json(true);
+        } 
 
-    return res.json(false);
+        return res.json(false);
+    } catch(err) {
+        next(err);
+    }
 }
 
-export { validate };
+const startGame = (req: Request, res: Response) => {
+    req.session.startTime = Date.now();
+    return res.json("Game started");
+}
+
+const endGame = async (req: Request, res: Response) => {
+    const start = req.session.startTime;
+    if (!start) return res.status(400).json({error: "Game not started"});
+
+    const name = req.body.name;
+    await prisma.score.create({
+        data: {
+            name, 
+            sid: req.sessionID,
+            time: Date.now() - start
+        }
+    });
+    return res.json("Game ended");
+}
+
+export { validate, startGame, endGame };
--- apps/backend/src/routes/game.router.ts
+++ apps/backend/src/routes/game.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { validate } from "../controllers/game.controller";
+import { endGame, startGame, validate } from "../controllers/game.controller";
 
 const gameRouter = Router();
 
 gameRouter.post('/validate', validate);
+gameRouter.post('/start-game', startGame);
+gameRouter.post('/end-game', endGame);
 
 export default gameRouter;
