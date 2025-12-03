61ba8a93436fe27b2df31434f6ff3e9619ed588f
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-03 15:44:12 +0200

    Allow name with symbols

--- apps/backend/src/controllers/score.controller.ts
+++ apps/backend/src/controllers/score.controller.ts
@@ -4,7 +4,7 @@ import { prisma } from "../prisma/client";
 
 const updateScore = [
         param('id').isAlphanumeric().withMessage('Id is required'),
-        body('name').trim().isAlphanumeric().withMessage('Name is required'),
+        body('name').trim().notEmpty().withMessage('Name is required'),
         async (req: Request, res: Response) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
