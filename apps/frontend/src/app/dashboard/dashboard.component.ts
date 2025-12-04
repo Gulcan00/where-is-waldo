d77710ebef075ffd92dbff7cd9d3fbd5a7bc6a3b
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-04 10:08:04 +0200

    Add leaderboard to dashboard. Update puzzle card styling.

--- apps/frontend/src/app/dashboard/dashboard.component.ts
+++ apps/frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { ApiService } from '../services/api.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Score } from '../models/score';
+import { TimerService } from '../services/timer.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,4 +14,13 @@ import { RouterLink } from "@angular/router";
 })
 export class DashboardComponent {
   scenes = ['scene1', 'scene2', 'scene3'];
+
+  scores: Signal<Score[]> = toSignal(this.api.getScores());
+
+  constructor (
+    private api: ApiService,
+    public timer: TimerService
+  ) {}
+
+
 }
--- apps/frontend/src/app/services/timer.service.ts
+++ apps/frontend/src/app/services/timer.service.ts
@@ -9,10 +9,7 @@ export class TimerService implements OnDestroy {
   isRunning = signal(false);
   formattedTime = computed(() => {
     const totalSeconds = this.time();
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return this.formatTime(totalSeconds);
   });
     
 
@@ -33,6 +30,13 @@ export class TimerService implements OnDestroy {
       clearInterval(this.intervalId);
       this.intervalId = null;
     }
+  }
+
+  formatTime(totalSeconds: number) {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 
   ngOnDestroy(): void {
